4f155e6f59bebf5e3b4c8b8b2de4d0d1b939fb46
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-11 17:56:26 +0100

    Offer an alternative way to add the TOC to a document in Markdown
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- gfm-add-toc.js
+++ gfm-add-toc.js
@@ -1,18 +1,4 @@
 (function() {
-  /* Obtain markdown from Edit box */
-  var textarea = document.getElementById('gollum-editor-body');
-  var md = textarea.innerHTML;
-
-  if (!md) {
-    console.log('No markdown editor found!');
-    return;
-  }
-
-  /* Strip out existing TOC, if any */
-  var tocStart = '** Table of contents **\n\n';
-  if (md.startsWith(tocStart))
-    md = md.substring(tocStart.length).replace(/[^]*?\n\n/m, '');
-
   /* Helper to generate the Table of Contents entries */
   var toPlainText = function(list) {
     var result = '';
@@ -31,22 +17,58 @@
     return result;
   };
 
-  /* Generate the Table of Contents */
-  var parsed = window.markdown.parse(md);
-  var toc = tocStart;
-  parsed.forEach(function(el) {
-    if (el instanceof Array && el[0] == 'header') {
-      for (var i = 1; i < el[1].level; i++)
-        toc += '\t';
-      var plainText = toPlainText(el.slice(2));
-      var anchor = plainText.toLowerCase()
-        .replace(/ /g, '-')
-        .replace(/[^-A-Za-z0-9]/g, '');
-      toc += '* [' + plainText + '](#' + anchor + ')\n';
+  var insertTOC = function(textarea) {
+    if (!textarea) {
+      console.log('No markdown editor found!');
+      return;
     }
-  });
-  toc += '\n';
 
-  /* Insert the Table of Contents */
-  textarea.innerHTML = toc + md;
+    /* Obtain markdown from Edit box */
+    var md = textarea.innerHTML || textarea.value;
+
+    /* Strip out existing TOC, if any */
+    var tocStart = '** Table of contents **\n\n';
+    if (md.startsWith(tocStart))
+      md = md.substring(tocStart.length).replace(/[^]*?\n\n/m, '');
+
+    /* Generate the Table of Contents */
+    var parsed = window.markdown.parse(md);
+    var toc = tocStart;
+    parsed.forEach(function(el) {
+      if (el instanceof Array && el[0] == 'header') {
+        for (var i = 1; i < el[1].level; i++)
+          toc += '\t';
+        var plainText = toPlainText(el.slice(2));
+        var anchor = plainText.toLowerCase()
+          .replace(/ /g, '-')
+          .replace(/[^-A-Za-z0-9]/g, '');
+        toc += '* [' + plainText + '](#' + anchor + ')\n';
+      }
+    });
+    toc += '\n';
+
+    /* Insert the Table of Contents */
+    textarea.value = toc + md;
+  };
+
+  var textarea = document.getElementById('gollum-editor-body');
+  if (textarea)
+    /* We are running as a bookmarklet */
+    insertTOC(textarea);
+  else {
+    var button = document.getElementById('insert-toc-expand');
+    textarea = document.getElementById('insert-toc-textarea');
+    if (button && textarea) {
+      button.onclick = function() {
+        textarea.style.display = 'block';
+      };
+
+      textarea.onpaste = function(e) {
+        setTimeout(function() {
+          insertTOC(textarea);
+          textarea.select();
+        }, 50);
+      };
+    }
+  }
 })();
